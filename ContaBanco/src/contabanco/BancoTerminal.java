c899456353376acc8b4fbb2e2467bef4e195a83c
crist <crist@DESKTOP-JSSE25O>
2024-01-17 22:38:44 -0300

    Exercício conta bancária Primeira parte console deve apresentar a mensagem contendo o saldo

--- ContaBanco/src/contabanco/BancoTerminal.java
+++ ContaBanco/src/contabanco/BancoTerminal.java
@@ -1,13 +1,26 @@
 
 package contabanco;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 
 public class BancoTerminal {
 
  
     public static void main(String[] args) {
         
-       
-    }
-    
-}
+        double saldo = 25;
+        double valorSolicitado = 18;       
+        
+        if (saldo>=valorSolicitado)
+            saldo = saldo - valorSolicitado;
+        else 
+            System.out.println("Saldo insuficiente");
+        
+            System.out.println("O saldo atualizado é: " + saldo);
+        
+        
+                    }
+                }
+
